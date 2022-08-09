41b8a9ffcd7e5f0c4c184839700e198e20c3a850
kfil <koafil@gmail.com>
2022-08-09 13:29:48 +0300

    опции в конфиге development и production

--- build/bundle.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  // src/second.js
-  function getName() {
-    return "Test name";
-  }
-
-  // src/index.js
-  console.log(getName());
-})();
--- config/build/esbuild-config.js
+++ config/build/esbuild-config.js
@@ -1,9 +1,14 @@
 const ESBuild = require('esbuild');
 const path = require('path');
 
+const mode  = process.env.MODE || 'development';
+const isDev = mode === 'development';
+const isProd = mode === 'production'
+
 ESBuild.build({
     outdir: path.resolve(__dirname,'..','..','build'),
     entryPoints: [path.resolve(__dirname,'..','..','src','index.js')],
     entryNames: 'bundle',
-    bundle: true
+    bundle: true,
+    minify: isProd
 });
